f6808707aea211a4bb09145d2ad38a1c93abfd3f
sswaroop-deng <sswaroop@umd.edu>
2025-09-16 13:54:29 -0400

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def hello_world():
- return 'Hello, World! from Swaroop Sreedhar'
-### your python code goes here
-###
-if __name__ == '__main__':
- app.run(debug=True)
